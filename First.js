4fc596a0aa49140ac97180ef1f583c12bec2f03e
Aqueum <martin@aqueum.com>
2017-07-08 23:43:13 +0100

    Move text from js to md

--- First.js
+++ First.js
@@ -2,70 +2,47 @@
  * Created by martin on 08/07/2017.
  */
 
-// console.log("hiya friend");
-//
-// for  (var i = 0; i < 10; i++) {
-//     console.log(i);
-// }
-//
-// // single line comment
-//
-// /*
-// Multi
-// Line
-// Comment
-//  */
-//
-// console.log("concatenate" + " " + "strings");
-//
-// // implicit type coercion
-// console.log("Hello" + 5*10)
-// // gives Hello50
-//
-// var variableName = "value";
-//
-// /*
-// https://google.github.io/styleguide/jsguide.html
-// styleguide says camelCase for variables
-// */
-//
-// console.log("James"[0]);
-//
-// console.log("This is how we say \"he said\", you see? \nand take a new line \n\ttabbed 2 \n\tor 3");
-//
-// // null is nothing, undefined is the absence of anything
-//
-// /*
-// && AND
-// || OR
-// ! NOT
-// */
-//
-// /*
-// Falsy values evaluate to false in context of boolean expression:
-// 1. the Boolean value false
-// 2. the null type
-// 3. the undefined type
-// 4. the number 0
-// 5. the empty string ""
-// 6. the odd value NaN (stands for "not a number", check out the NaN MDN article)
-// Everything else is Truthy
-// */
-//
-// //Ternary operator
-// //conditional ? (if confition is true) : (if condition is false)
-//
-// var isGoing = true;
-// var colour = isGoing ? "green" : "red";
-// console.log(colour);
-//
-// // is equivalent to:
-//
-// var isGoing = true;
-// var color;
-// if (isGoing) {
-//     color = "green";
-// } else {
-//     color = "red";
-// }
-// console.log(color);
+console.log("hiya friend");
+
+for  (var i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+// single line comment
+
+/*
+Multi
+Line
+Comment
+ */
+
+console.log("concatenate" + " " + "strings");
+
+// implicit type coercion
+console.log("Hello" + 5*10);
+// gives Hello50
+
+var variableName = "value";
+console.log(variableName);
+
+console.log("James"[0]);
+
+console.log("This is how we say \"he said\", you see? \nand take a new line \n\ttabbed 2 \n\tor 3");
+
+//Ternary operator
+//conditional ? (if condition is true) : (if condition is false)
+
+var isGoing = true;
+var colour = isGoing ? "green" : "red";
+console.log(colour);
+
+// is equivalent to:
+
+var isGoing2 = true;
+var color;
+if (isGoing2) {
+    color = "green";
+} else {
+    color = "red";
+}
+console.log(color);
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # UFS-JSintro
 Notes from udacity [UD803 Intro to JavaScript](https://www.udacity.com/course/intro-to-javascript--ud803)
+
+##Values
+- null is nothing
+- undefined is the absence of anything
+- NaN is not a number
+
+##Boolian Operators
+- &&    = AND
+- ||    = OR
+- !     = NOT
+
+##Style
+[styleguide](https://google.github.io/styleguide/jsguide.html
+) says camelCase for variables
+
+
+##Falsy & Truthy
+Falsy values evaluate to false in context of boolean expression:
+1. the Boolean value false
+2. the null type
+3. the undefined type
+4. the number 0
+5. the empty string ""
+6. the odd value NaN (stands for "not a number", check out the NaN MDN article)
+
+Everything else is Truthy
+
